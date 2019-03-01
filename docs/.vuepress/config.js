f41cb2c2e7d1fbbd8f958430847a707d3e8f70d7
zhaoshuan <zhashuan@kkworld.com>
2019-03-01 20:43:56 +0800

    创建vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    title: 'demo', 
+    description: '这是一个简介',
+    port: 8092,
+    serviceWorker: true,
+    themeConfig:{
+        nav:[{
+            text: 'github',
+            link: 'https://github.com/zhaoshuan/webpack4-learning'
+        }],
+        sidebar:['/zhao/']
+    },
+}
